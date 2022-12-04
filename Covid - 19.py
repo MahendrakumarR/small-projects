61ae04c740ce1f917ce9a08f4a24f21bfd05906b
Mahendrakumar <72775190+MahendrakumarR@users.noreply.github.com>
2022-12-04 17:09:15 +0530

    Add files via upload

---
+++ Covid - 19.py
@@ -0,0 +1,275 @@
+print("\n Covid-19 Data Management")
+city=["Chennai","Coimbatore","Erode","Namakal","Salem","Kanniyakumari","Madurai","Thanjavur","Thirunelveli","Thiruvannamalai"]
+area=input("\n Enter the city name:")
+if area in city:
+    if area=="Erode":
+        print(f"\n welcome to {area} Covid Hospital")
+        case=input("\n Covid Cases(pasitive, negative, recovered, dead):")
+        if case=="pasitive":
+            print("\n-----Pasitive Cases-----")
+            print("\n Per day = 8")
+            print("\n Yesterday = 10")
+            print("\n Today = 18")
+        elif case=="negative":
+            print("\n-----Negative Cases------")
+            print("\n Per day = 6")
+            print("\n Yesterday = 4")
+            print("\n Today = 2")
+        elif case=="recovered":
+            print("\n-----Recovered Cases-----")
+            print("\n Per day = 5")
+            print("\n Yesterday = 3")
+            print("\n Today = 1")
+        elif case=="dead":
+            print("\n-----Dead Cases------")
+            print("\n Per day = 11")
+            print("\n Yesterday = 7")
+            print("\n Today = 9")
+        else:
+            print("\n Type correct cases")
+    
+
+elif area=="Chennai":
+    print(f"\n welcome to {area} Covid Hospital")
+    case=input("\n Covid Cases(pasitive, negative, recovered, dead):")
+    if case=="pasitive":
+        print("\n-----Pasitive Cases-----")
+        print("\n Per day = 20")
+        print("\n Yesterday = 10")
+        print("\n Today = 11")
+    elif case=="negative":
+        print("\n-----Negative Cases------")
+        print("\n Per day = 22")
+        print("\n Yesterday = 12")
+        print("\n Today = 10")
+    elif case=="recovered":
+        print("\n-----Recovered Cases-----")
+        print("\n Per day = 28")
+        print("\n Yesterday = 13")
+        print("\n Today = 16")
+    elif case=="dead":
+        print("\n-----Dead Cases------")
+        print("\n Per day = 18")
+        print("\n Yesterday = 10")
+        print("\n Today = 9")
+    else:
+        print("\n Type correct cases")
+
+
+elif area=="Coimbatore":
+    print(f"\n welcome to {area} Covid Hospital")
+    case=input("\n Covid Cases(pasitive, negative, recovered, dead):")
+    if case=="pasitive":
+        print("\n-----Pasitive Cases-----")
+        print("\n Per day = 21")
+        print("\n Yesterday = 10")
+        print("\n Today = 18")
+    elif case=="negative":
+        print("\n-----Negative Cases------")
+        print("\n Per day = 16")
+        print("\n Yesterday = 8")
+        print("\n Today = 12")
+    elif case=="recovered":
+        print("\n-----Recovered Cases-----")
+        print("\n Per day = 10")
+        print("\n Yesterday = 3")
+        print("\n Today = 4")
+    elif case=="dead":
+        print("\n-----Dead Cases------")
+        print("\n Per day = 15")
+        print("\n Yesterday = 11")
+        print("\n Today = 10")
+    else:
+        print("\n Type correct cases")
+
+
+elif area=="Salem":
+    print(f"\n welcome to {area} Covid Hospital")
+    case=input("\n Covid Cases(pasitive, negative, recovered, dead):")
+    if case=="pasitive":
+        print("\n-----Pasitive Cases-----")
+        print("\n Per day = 8")
+        print("\n Yesterday = 10")
+        print("\n Today = 18")
+    elif case=="negative":
+        print("\n-----Negative Cases------")
+        print("\n Per day = 6")
+        print("\n Yesterday = 4")
+        print("\n Today = 2")
+    elif case=="recovered":
+        print("\n-----Recovered Cases-----")
+        print("\n Per day = 5")
+        print("\n Yesterday = 3")
+        print("\n Today = 1")
+    elif case=="dead":
+        print("\n-----Dead Cases------")
+        print("\n Per day = 11")
+        print("\n Yesterday = 7")
+        print("\n Today = 9")
+    else:
+        print("\n Type correct cases")
+
+
+elif area=="Namakal":
+    print(f"\n welcome to {area} Covid Hospital")
+    case=input("\n Covid Cases(pasitive, negative, recovered, dead):")
+    if case=="pasitive":
+        print("\n-----Pasitive Cases-----")
+        print("\n Per day = 8")
+        print("\n Yesterday = 10")
+        print("\n Today = 18")
+    elif case=="negative":
+        print("\n-----Negative Cases------")
+        print("\n Per day = 6")
+        print("\n Yesterday = 4")
+        print("\n Today = 2")
+    elif case=="recovered":
+        print("\n-----Recovered Cases-----")
+        print("\n Per day = 5")
+        print("\n Yesterday = 3")
+        print("\n Today = 1")
+    elif case=="dead":
+        print("\n-----Dead Cases------")
+        print("\n Per day = 11")
+        print("\n Yesterday = 7")
+        print("\n Today = 9")
+    else:
+        print("\n Type correct cases")
+
+
+elif area=="Kanniyakumari":
+    print(f"\n welcome to {area} Covid Hospital")
+    case=input("\n Covid Cases(pasitive, negative, recovered, dead):")
+    if case=="pasitive":
+        print("\n-----Pasitive Cases-----")
+        print("\n Per day = 8")
+        print("\n Yesterday = 10")
+        print("\n Today = 18")
+    elif case=="negative":
+        print("\n-----Negative Cases------")
+        print("\n Per day = 6")
+        print("\n Yesterday = 4")
+        print("\n Today = 2")
+    elif case=="recovered":
+        print("\n-----Recovered Cases-----")
+        print("\n Per day = 5")
+        print("\n Yesterday = 3")
+        print("\n Today = 1")
+    elif case=="dead":
+        print("\n-----Dead Cases------")
+        print("\n Per day = 11")
+        print("\n Yesterday = 7")
+        print("\n Today = 9")
+    else:
+        print("\n Type correct cases")
+
+elif area=="Madurai":
+    print(f"\n welcome to {area} Covid Hospital")
+    case=input("\n Covid Cases(pasitive, negative, recovered, dead):")
+    if case=="pasitive":
+        print("\n-----Pasitive Cases-----")
+        print("\n Per day = 8")
+        print("\n Yesterday = 10")
+        print("\n Today = 18")
+    elif case=="negative":
+        print("\n-----Negative Cases------")
+        print("\n Per day = 6")
+        print("\n Yesterday = 4")
+        print("\n Today = 2")
+    elif case=="recovered":
+        print("\n-----Recovered Cases-----")
+        print("\n Per day = 5")
+        print("\n Yesterday = 3")
+        print("\n Today = 1")
+    elif case=="dead":
+        print("\n-----Dead Cases------")
+        print("\n Per day = 11")
+        print("\n Yesterday = 7")
+        print("\n Today = 9")
+    else:
+        print("\n Type correct cases")
+
+
+elif area=="Thirunelveli":
+    print(f"\n welcome to {area} Covid Hospital")
+    case=input("\n Covid Cases(pasitive, negative, recovered, dead):")
+    if case=="pasitive":
+        print("\n-----Pasitive Cases-----")
+        print("\n Per day = 8")
+        print("\n Yesterday = 10")
+        print("\n Today = 18")
+    elif case=="negative":
+        print("\n-----Negative Cases------")
+        print("\n Per day = 6")
+        print("\n Yesterday = 4")
+        print("\n Today = 2")
+    elif case=="recovered":
+        print("\n-----Recovered Cases-----")
+        print("\n Per day = 5")
+        print("\n Yesterday = 3")
+        print("\n Today = 1")
+    elif case=="dead":
+        print("\n-----Dead Cases------")
+        print("\n Per day = 11")
+        print("\n Yesterday = 7")
+        print("\n Today = 9")
+    else:
+        print("\n Type correct cases")
+
+
+elif area=="Thanjavur":
+    print(f"\n welcome to {area} Covid Hospital")
+    case=input("\n Covid Cases(pasitive, negative, recovered, dead):")
+    if case=="pasitive":
+        print("\n-----Pasitive Cases-----")
+        print("\n Per day = 8")
+        print("\n Yesterday = 10")
+        print("\n Today = 18")
+    elif case=="negative":
+        print("\n-----Negative Cases------")
+        print("\n Per day = 6")
+        print("\n Yesterday = 4")
+        print("\n Today = 2")
+    elif case=="recovered":
+        print("\n-----Recovered Cases-----")
+        print("\n Per day = 5")
+        print("\n Yesterday = 3")
+        print("\n Today = 1")
+    elif case=="dead":
+        print("\n-----Dead Cases------")
+        print("\n Per day = 11")
+        print("\n Yesterday = 7")
+        print("\n Today = 9")
+    else:
+        print("\n Type correct cases")
+
+
+elif area=="Thiruvannamalai":
+    print(f"\n welcome to {area} Covid Hospital")
+    case=input("\n Covid Cases(pasitive, negative, recovered, dead):")
+    if case=="pasitive":
+        print("\n-----Pasitive Cases-----")
+        print("\n Per day = 8")
+        print("\n Yesterday = 10")
+        print("\n Today = 18")
+    elif case=="negative":
+        print("\n-----Negative Cases------")
+        print("\n Per day = 6")
+        print("\n Yesterday = 4")
+        print("\n Today = 2")
+    elif case=="recovered":
+        print("\n-----Recovered Cases-----")
+        print("\n Per day = 5")
+        print("\n Yesterday = 3")
+        print("\n Today = 1")
+    elif case=="dead":
+        print("\n-----Dead Cases------")
+        print("\n Per day = 11")
+        print("\n Yesterday = 7")
+        print("\n Today = 9")
+    else:
+        print("\n Type correct cases")
+else:
+    print("\n Write correctly the city names")
+
+
